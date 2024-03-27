fb8c609c133c244a7818dca10a5a12ea887e6303
weeezik <mkihanya.33@gmail.com>
2024-03-27 16:34:25 +0000

    Comments on initial ideas on how to solve

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,1 +1,15 @@
 num_array = [4, 3, 78, 2, 0, 2]
+
+def bubble_sort([2, 1, 3])
+  #Steps
+  #selects two number to compare
+    # if num[index] < num[index+1]
+      #move to next two next
+    #else
+      #the numbers switch positions
+      #num[index].index = num[index+1]
+      #num[index+1].index = num[index]
+  
+  
+  # using each method, compare to number +1 index above, return the greater one
+end
