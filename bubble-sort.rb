0d93e18f42c1384bae664b183e2c1d496ecf8962
weeezik <mkihanya.33@gmail.com>
2024-03-30 19:10:21 +0200

    Write notes on bottom of ruby file
    For next steps and things to consider. Still need to
    confirm the looping work and returning the new array
    without errors.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,27 +1,36 @@
+require 'pry-byebug'
+
 num_array = [4, 3, 78, 2, 0, 2]
 test_array = [2, 1, 3]
 def bubble_sort(unsorted_array)
   array_length = unsorted_array.length # how many times to loop
   # foreachwithindex
-  # unsorted_array[0] < unsorted_array[0 + 1] (continue case, add one to i)
-    # i++ then next iteration of loop
-  # unsorted_array[0] > unsorted_array[0+1] (switch then continue case)
-    q_nums_array = unsorted_array.slice!(i..i+1)
-    q_nums_array.reverse!
-    step_sort_array = q_nums_array + unsorted_array
-    # unsorted_array[0].index = 0+1 
-    # unsorted_array[0+1].index = 0
-          # return new 2-numbered array using slice, then reverse those element. then push them back into array in reverse order
-    # i++ then next iteration of loop
-  # Steps
-  # selects two number to compare
-  # if num[index] < num[index+1]
-  # move to next two next
-  # else
-  # the numbers switch positions
-  # num[index].index = num[index+1]
-  # num[index+1].index = num[index]
-  # using each method, compare to number +1 index above, return the greater one
+  i = 0
+  while i < array_length
+    num1 = unsorted_array[i]
+    num2 = unsorted_array[i + 1]
+    # binding.pry
+    if num1 < num2 # continue case
+      i += 1
+      next
+    elsif num1 > num2 # switch then continue case
+      q_nums_array = unsorted_array.slice!(i..i + 1)
+      q_nums_array.reverse!
+      step_sort_array = q_nums_array + unsorted_array
+       i += 1
+      puts step_sort_array
+      # binding.pry
+    else
+      puts 'ERROR'
+    end
+  end
 end
 
 puts bubble_sort(test_array)
+
+# still need to have a cancel logic (i.e., when the array is in order, 
+# stop the process and return the array)
+
+#need to loop through every element of the array eAcH tImE nO mAtTeR wHaT AND on a higher level, 
+# loop through the array X amount of times until each comparison returns true a number of times equal
+# to the length of the array
