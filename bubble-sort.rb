03628b9fe58224c51c566c6cf75ca99e1b7e2795
weeezik <mkihanya.33@gmail.com>
2024-03-30 17:55:43 +0200

    Delete previous half-done if statement
    Starting from scratch with the logic I wrote
    out in the comments. Using the CS50 video and
    wikipedia pages as reference.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,15 +1,16 @@
 num_array = [4, 3, 78, 2, 0, 2]
-
-def bubble_sort([2, 1, 3])
-  #Steps
-  #selects two number to compare
-    # if num[index] < num[index+1]
-      #move to next two next
-    #else
-      #the numbers switch positions
-      #num[index].index = num[index+1]
-      #num[index+1].index = num[index]
-  
-  
+test_array = [2, 1, 3]
+def bubble_sort(unsorted_array)
+  puts unsorted_array.join(" ")
+  # Steps
+  # selects two number to compare
+  # if num[index] < num[index+1]
+  # move to next two next
+  # else
+  # the numbers switch positions
+  # num[index].index = num[index+1]
+  # num[index+1].index = num[index]
   # using each method, compare to number +1 index above, return the greater one
 end
+
+puts bubble_sort(test_array)
