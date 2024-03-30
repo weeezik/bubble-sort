d8865edde02c631ef82a774ff584a8b8fa8e82db
weeezik <mkihanya.33@gmail.com>
2024-03-30 18:12:27 +0200

    Pseudocode fully written
    Need to add proper loops and index variable for proper iteration.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,7 +1,16 @@
 num_array = [4, 3, 78, 2, 0, 2]
 test_array = [2, 1, 3]
 def bubble_sort(unsorted_array)
-  puts unsorted_array.join(" ")
+  array_length = unsorted_array.length # how many times to loop
+  # foreachwithindex
+  # unsorted_array[0] < unsorted_array[0 + 1] (continue case, add one to i)
+    # i++ then next iteration of loop
+  # unsorted_array[0] > unsorted_array[0+1] (switch then continue case)
+    # unsorted_array[0].index = 0+1 
+    # unsorted_array[0+1].index = 0
+          # return new 2-numbered array using slice, then reverse those element. then push them back into array in reverse order
+    # i++ then next iteration of loop
+  puts unsorted_array.join(' ')
   # Steps
   # selects two number to compare
   # if num[index] < num[index+1]
