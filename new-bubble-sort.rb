84b32849c0e2221d7c6a172a96ca7a708e2c9814
weeezik <mkihanya.33@gmail.com>
2024-06-07 14:00:44 -0700

    Can iterate once over the array But it does not continue iterating until the array is completely sorted.

--- new-bubble-sort.rb
+++ new-bubble-sort.rb
@@ -1,18 +1,14 @@
-def bubble_sort (unsorted_numbers)
+require 'pry-byebug'
+
+def bubble_sort(unsorted_numbers)
   unsorted_numbers.each_with_index do |current_number, index|
     next_number = unsorted_numbers[index + 1]
-    # while unsorted_numbers ==! unsorted_numbers.sort
-    if next_number.is_a? Numeric
-      if current_number > next_number
-        unsorted_numbers[index] = next_number
-        unsorted_numbers[index+1] = current_number
-      else
-        return unsorted_numbers
-      end
-    else
-      return unsorted_numbers
-    end
+    return unsorted_numbers unless next_number.is_a? Numeric
+    return unsorted_numbers unless current_number > next_number
+
+    unsorted_numbers[index] = next_number
+    unsorted_numbers[index + 1] = current_number
   end
 end
 
-p bubble_sort ([6, 3, 2])
+puts bubble_sort([6, 3, 2])
