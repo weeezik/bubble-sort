03d1fb217b869b5e9e28f71afcf0c77fb7fe808e
weeezik <mkihanya.33@gmail.com>
2024-06-07 12:43:54 -0700

    New bubble sort file added.

---
+++ new-bubble-sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort (unsorted_numbers)
+  unsorted_numbers.each_with_index do |current_number, index|
+    next_number = unsorted_numbers[index + 1]
+    if next_number.is_a? Numeric
+      if current_number > next_number
+        unsorted_numbers[index] = next_number
+        unsorted_numbers[index+1] = current_number
+      else
+        return unsorted_numbers
+      end
+    else
+      return unsorted_numbers
+    end
+  end
+end
+
+p bubble_sort ([6, 3])
