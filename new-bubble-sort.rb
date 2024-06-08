0d82f22bd7f8bc56e22b59f938118abb5663c7d4
weeezik <mkihanya.33@gmail.com>
2024-06-08 11:35:12 -0700

    Complete bubble sort task
    Break line:  #indicates the end of one iteration sorting through the array
    Next line: #skips to next iteration of loop if the
                current and next number are already in their correct positions
    Before I had a break here, which exited the iteration of the array entirely,
    instead of just skipping the current pair of numbers to compare the next pair
    of numbers.
    
    I did it! Let's go!!!

--- new-bubble-sort.rb
+++ new-bubble-sort.rb
@@ -1,15 +1,22 @@
 require 'pry-byebug'
 
 def bubble_sort(unsorted_numbers)
-  unsorted_numbers.each_with_index do |current_number, index|
-    next_number = unsorted_numbers[index + 1]
-    return unsorted_numbers unless next_number.is_a? Numeric
-    return unsorted_numbers unless current_number > next_number
-
-    unsorted_numbers[index] = next_number
-    unsorted_numbers[index + 1] = current_number
+  sorted_numbers = []
+  element_count = unsorted_numbers.length
+  element_count.times do
+    unsorted_numbers.each_with_index do |current_number, index|
+      next_number = unsorted_numbers[index + 1]
+      break unless next_number.is_a? Numeric
+      #indicates the end of one iteration sorting through the array
+      next unless current_number > next_number
+      #skips to next iteration of loop if the current and next number are already in their correct positions
+      #before I had a break here, which exiting the iteration of the array entirely, instead of just skipping the next pair of numbers to compare
+      unsorted_numbers[index] = next_number
+      unsorted_numbers[index + 1] = current_number
+    end
+    sorted_numbers.unshift(unsorted_numbers.pop)
   end
-  return unsorted_numbers
+  return sorted_numbers
 end
 
-puts bubble_sort([6, 3, 2])
+p bubble_sort([4,3,78,2,0,2])
